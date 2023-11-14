fe85862ebfa057324428c34c1fdc2c4a08e2150d
Kritiksharma004 <146127433+Kritiksharma004@users.noreply.github.com>
2023-11-13 21:58:14 -0600

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,12 +1,6 @@
 const envType = process.env.REACT_APP_ENV || "development";
 
 const types = {
-    development: {
-        BASE_URL: "http://localhost:8080/api/",
-    },
-    staging: {
-        BASE_URL: "https://stage.test.com/",
-    },
     production: {
         BASE_URL: "https://restaurant-booking-backend-steel.vercel.app/api/",
     }
